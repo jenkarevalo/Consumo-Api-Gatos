f4b5eb2a432ceaa2092919565eb9361c0eafa5c0
Jenny Katherine Arevalo <jenkarevalo@gmail.com>
2024-03-04 15:55:21 -0500

    Primer commit Consuno de Api

--- src/App.jsx
+++ src/App.jsx
@@ -2,54 +2,48 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [catFact, setCatFact] = useState(''); // Use more descriptive name
-  const [catImageUrl, setCatImageUrl] = useState('');
+  const [booksFact, setBooksFact] = useState(''); // Use more descriptive name
+  const [booksImageUrl, setBooksImageUrl] = useState('');
 
   useEffect(() => {
-    const fetchCatFact = async () => {
+    const fetchBooksFact = async () => {
       try {
-        const response = await fetch('https://catfact.ninja/fact');
+        const response = await fetch('https://openlibrary.org/authors/OL1394244A/works.json?offset=50');
         const data = await response.json();
-        setCatFact(data.fact);
+        setBooksFact(data.title);
+      } catch (error) {
+        console.error('Error fetching books fact:', error);
+      }
+    };
+    fetchBooksFact();
+  }, []);
+
+  useEffect(() => {
+    const generateBooksImage = async () => {
+      try {
+        const response = await fetch('https://source.unsplash.com/featured/?libros');
+        if (response.ok) {
+          const data = await response.blob(); // Handle data as blob
+          const imageUrl = URL.createObjectURL(data); // Generate URL from blob
+          setBooksImageUrl(imageUrl);
+        } else {
+          console.error('Error fetching books image:', response.statusText);
+        }
       } catch (error) {
         console.error(error);
       }
     };
-
-    fetchCatFact();
-  }, []);
-
-  useEffect(() => {
-    if (catFact) {
-      const generateCatImage = async () => {
-        try {
-          const firstWord = catFact.split(' ', 3).join(' ');
-          const response = await fetch(`https://cataas.com/cat/says/${firstWord}`);
-
-          if (response.ok) {
-            const data = await response.blob(); // Handle data as blob
-            const imageUrl = URL.createObjectURL(data); // Generate URL from blob
-            setCatImageUrl(imageUrl);
-          } else {
-            console.error('Error fetching cat image:', response.statusText);
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      };
-
-      generateCatImage();
-    }
-  }, [catFact]);
+    generateBooksImage();
+  }, [booksFact]);
 
   return (
     <>
-      <h1>Random Cat Fact</h1>
-      {catFact && <p>{catFact}</p>}
-      {catImageUrl && (
-        <img src={catImageUrl} alt="random cat" width={300} height={300} />
+      <h1>Random Books</h1>
+      {booksFact && <p>{booksFact}</p>}
+      {booksImageUrl && (
+        <img src={booksImageUrl} alt="random books" width={300} height={300} />
       )}
-      {catImageUrl && <p>Image generated based on the first word of the fact.</p>}
+      {booksImageUrl && <p>Image generated based on the first word of the fact.</p>}
     </>
   );
 }
